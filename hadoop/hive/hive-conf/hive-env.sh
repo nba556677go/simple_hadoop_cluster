6f45754eb803daf06303c559b69851adb87954fb
nba556677go <nba556677@gmail.com>
2020-10-08 21:39:23 +0800

    remove hive config

--- hadoop/hive/hive-conf/hive-env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export HADOOP_HOME=/opt/hadoop-2.7.3
-export HIVE_HOME=/opt/apache-hive-2.3.4-bin
-export HIVE_CONF_DIR=/opt/apache-hive-2.3.4-bin/conf
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
